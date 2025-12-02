d1f26d0457ae9b15c4e11226711415fb7af18f5d
Alisson Lamarque <alissonlamarque.si@gmail.com>
2025-12-02 16:04:38 -0300

    Criação inicial do projeto

---
+++ src/main/java/org/example/trabalhofinal_trezedemaio/TrabalhoFinalTrezeDeMaioApplication.java
@@ -0,0 +1,13 @@
+package org.example.trabalhofinal_trezedemaio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoFinalTrezeDeMaioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoFinalTrezeDeMaioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/trabalhofinal_trezedemaio/TrabalhoFinalTrezeDeMaioApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.trabalhofinal_trezedemaio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoFinalTrezeDeMaioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
