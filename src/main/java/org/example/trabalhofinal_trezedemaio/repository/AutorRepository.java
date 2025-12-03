227d10cbdd58a8401b9c976e1137346c6040be17
Alisson Lamarque <alissonlamarque.si@gmail.com>
2025-12-03 17:43:44 -0300

    Criação dos arquivos repository

---
+++ src/main/java/org/example/trabalhofinal_trezedemaio/repository/AutorRepository.java
@@ -0,0 +1,9 @@
+package org.example.trabalhofinal_trezedemaio.repository;
+
+import org.example.trabalhofinal_trezedemaio.model.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+}
